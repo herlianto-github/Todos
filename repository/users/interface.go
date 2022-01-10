3bc6ab7f52a3dc8999dfb7b0bdc124519607a3da
andrewptjio <andrewptjioo@gmail.com>
2022-01-10 16:56:34 +0800

    repo user

--- repository/users/interface.go
+++ repository/users/interface.go
@@ -3,5 +3,9 @@ package users
 import "todos/entities"
 
 type UsersInterface interface {
-	Gets() ([]entities.User, error)
+	GetAll() ([]entities.User, error)
+	Get(userId int) (entities.User, error)
+	Create(user entities.User) (entities.User, error)
+	Delete(userId int) (entities.User, error)
+	Update(nweUser entities.User, userId int) (entities.User, error)
 }
--- repository/users/users.go
+++ repository/users/users.go
@@ -14,11 +14,33 @@ func NewUsersRepo(db *gorm.DB) *UserRepository {
 	return &UserRepository{db: db}
 }
 
-// /users GET
-func (usrep *UserRepository) Gets() ([]entities.User, error) {
-
+func (ur *UserRepository) GetAll() ([]entities.User, error) {
 	users := []entities.User{}
-	usrep.db.Find(&users)
-
+	ur.db.Find(&users)
 	return users, nil
+}
+
+func (ur *UserRepository) Get(userId int) (entities.User, error) {
+	user := entities.User{}
+	ur.db.Find(&user, userId)
+	return user, nil
+}
+
+func (ur *UserRepository) Create(user entities.User) (entities.User, error) {
+	ur.db.Save(&user)
+	return user, nil
+}
+
+func (ur *UserRepository) Delete(userId int) (entities.User, error) {
+	user := entities.User{}
+	ur.db.Find(&user, "id=?", userId)
+	ur.db.Delete(&user)
+	return user, nil
+}
+
+func (ur *UserRepository) Update(newUser entities.User, userId int) (entities.User, error) {
+	user := entities.User{}
+	ur.db.Find(&user, "id=?", userId)
+	ur.db.Model(&user).Updates(newUser)
+	return newUser, nil
 }

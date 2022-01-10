5883217068d0e8e3aec36c13d905c0789113b82b
andrewptjio <andrewptjioo@gmail.com>
2022-01-10 14:53:15 +0800

    to do DB initalization

--- repository/to_do/to_do.go
+++ repository/to_do/to_do.go
@@ -1,1 +1,13 @@
 package to_do
+
+import (
+	"gorm.io/gorm"
+)
+
+type To_DoRepository struct {
+	db *gorm.DB
+}
+
+func NewTo_DoRepo(db *gorm.DB) *To_DoRepository {
+	return &To_DoRepository{db: db}
+}

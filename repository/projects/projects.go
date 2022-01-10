5ef7fbe2abeff99226b6554dae1f07b8ae110e59
andrewptjio <andrewptjioo@gmail.com>
2022-01-10 14:57:17 +0800

    project DB init

--- repository/projects/projects.go
+++ repository/projects/projects.go
@@ -1,1 +1,13 @@
 package projects
+
+import (
+	"gorm.io/gorm"
+)
+
+type ProjectRepository struct {
+	db *gorm.DB
+}
+
+func NewProjectRepo(db *gorm.DB) *ProjectRepository {
+	return &ProjectRepository{db: db}
+}

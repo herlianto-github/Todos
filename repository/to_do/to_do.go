c9f4d205843eb2d1d041ad7b02402a51014a8913
andrewptjio <andrewptjioo@gmail.com>
2022-01-10 16:35:40 +0800

    to_do interface

--- repository/to_do/to_do.go
+++ repository/to_do/to_do.go
@@ -1,6 +1,8 @@
 package to_do
 
 import (
+	"todos/entities"
+
 	"gorm.io/gorm"
 )
 
@@ -10,4 +12,10 @@ type To_DoRepository struct {
 
 func NewTo_DoRepo(db *gorm.DB) *To_DoRepository {
 	return &To_DoRepository{db: db}
+}
+
+func (td *To_DoRepository) GetAll() ([]entities.To_Do, error) {
+	to_dos := []entities.To_Do{}
+	td.db.Find(&to_dos)
+	return to_dos, nil
 }

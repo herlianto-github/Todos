18ebf86fb89032989c719190868dfad7a94d76d4
andrewptjio <andrewptjioo@gmail.com>
2022-01-10 16:30:14 +0800

    interface repo to do

--- repository/to_do/interface.go
+++ repository/to_do/interface.go
@@ -1,1 +1,11 @@
 package to_do
+
+import "todos/entities"
+
+type To_Do interface {
+	GetAll() ([]entities.To_Do, error)
+	Get(toDoId int) (entities.To_Do, error)
+	Create(todo entities.To_Do) (entities.To_Do, error)
+	Delete(toDoId int) (entities.To_Do, error)
+	Update(newToDo entities.To_Do, toDoId int) (entities.To_Do, error)
+}

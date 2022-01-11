0eb32809c60de2c443e146349fd78d42c7e20bf7
asgard <asgard@Aphrodite.local>
2022-01-11 20:44:06 +0800

    fix typo

--- repository/to_do/interface.go
+++ repository/to_do/interface.go
@@ -2,7 +2,7 @@ package to_do
 
 import "todos/entities"
 
-type To_Do interface {
+type To_DoInterface interface {
 	GetAll() ([]entities.To_Do, error)
 	Get(toDoId int) (entities.To_Do, error)
 	Create(todo entities.To_Do) (entities.To_Do, error)
--- repository/to_do/to_do.go
+++ repository/to_do/to_do.go
@@ -14,33 +14,33 @@ func NewTo_DoRepo(db *gorm.DB) *To_DoRepository {
 	return &To_DoRepository{db: db}
 }
 
-func (td *To_DoRepository) GetAll() ([]entities.To_Do, error) {
+func (tdr *To_DoRepository) GetAll() ([]entities.To_Do, error) {
 	to_Dos := []entities.To_Do{}
-	td.db.Find(&to_Dos)
+	tdr.db.Find(&to_Dos)
 	return to_Dos, nil
 }
 
-func (td *To_DoRepository) Get(to_DoId int) (entities.To_Do, error) {
+func (tdr *To_DoRepository) Get(to_DoId int) (entities.To_Do, error) {
 	to_Do := entities.To_Do{}
-	td.db.Find(&to_Do, to_DoId)
+	tdr.db.Find(&to_Do, to_DoId)
 	return to_Do, nil
 }
 
-func (td *To_DoRepository) Create(to_Do entities.To_Do) (entities.To_Do, error) {
-	td.db.Save(&to_Do)
+func (tdr *To_DoRepository) Create(to_Do entities.To_Do) (entities.To_Do, error) {
+	tdr.db.Save(&to_Do)
 	return to_Do, nil
 }
 
-func (td *To_DoRepository) Delete(to_DoId int) (entities.To_Do, error) {
+func (tdr *To_DoRepository) Delete(to_DoId int) (entities.To_Do, error) {
 	to_Do := entities.To_Do{}
-	td.db.Find(&to_Do, "id=?", to_DoId)
-	td.db.Delete(&to_Do)
+	tdr.db.Find(&to_Do, "id=?", to_DoId)
+	tdr.db.Delete(&to_Do)
 	return to_Do, nil
 }
 
-func (td *To_DoRepository) Update(newTo_Do entities.To_Do, to_DoId int) (entities.To_Do, error) {
+func (tdr *To_DoRepository) Update(newTo_Do entities.To_Do, to_DoId int) (entities.To_Do, error) {
 	to_Do := entities.To_Do{}
-	td.db.Find(&to_Do, "id=?", to_DoId)
-	td.db.Model(&to_Do).Updates(newTo_Do)
+	tdr.db.Find(&to_Do, "id=?", to_DoId)
+	tdr.db.Model(&to_Do).Updates(newTo_Do)
 	return newTo_Do, nil
 }

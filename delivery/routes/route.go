46425bdab85f5f08a57937857cf661b27fd6ff3f
naufalaammar <hibatullah.nfl@gmail.com>
2022-01-12 10:47:23 +0700

    add route todo

--- delivery/routes/route.go
+++ delivery/routes/route.go
@@ -2,13 +2,16 @@ package routes
 
 import (
 	"todos/delivery/controllers/auth"
+	"todos/delivery/controllers/todo"
 	"todos/delivery/controllers/users"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
-func RegisterPath(e *echo.Echo, authctrl *auth.AuthController, uctrl *users.UsersController) {
+func RegisterPath(
+	e *echo.Echo, authctrl *auth.AuthController, uctrl *users.UsersController, toctrl *todo.ToDoController,
+) {
 
 	// ---------------------------------------------------------------------
 	// Login & Register
@@ -20,5 +23,14 @@ func RegisterPath(e *echo.Echo, authctrl *auth.AuthController, uctrl *users.User
 	// CRUD Users
 	// ---------------------------------------------------------------------
 	e.GET("/users", uctrl.GetUsersCtrl(), middleware.JWT([]byte("RAHASIA")))
+
+	// ---------------------------------------------------------------------
+	// CRUD Todo
+	// ---------------------------------------------------------------------
+	e.POST("/todo", toctrl.PostTodoCtrl())
+	e.GET("/todo", toctrl.GetAllTodoCtrl())
+	e.GET("/todssso", toctrl.GetTodoCtrl())
+	e.PUT("/todo", toctrl.PutTodoCtrl())
+	e.DELETE("/todo", toctrl.DeleteTodoCtrl())
 
 }

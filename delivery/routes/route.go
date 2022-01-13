3ad5bc78c689e16c1dc5069d2d4d211a7c675369
asgard <asgard@Aphrodite.local>
2022-01-14 00:08:38 +0800

    fix openapi

--- delivery/routes/route.go
+++ delivery/routes/route.go
@@ -45,7 +45,7 @@ func RegisterPath(e *echo.Echo, authctrl *auth.AuthController, uctrl *user.Users
 	e.POST("/projects", pctrl.PostProjectsCtrl(), middleware.JWT([]byte("RAHASIA")))
 	e.GET("/projects/all", pctrl.GetAllProjectsCtrl(), middleware.JWT([]byte("RAHASIA")))
 	e.GET("/projects", pctrl.GetProjectsCtrl(), middleware.JWT([]byte("RAHASIA")))
-	e.PUT("/projects", pctrl.PutProjectsCtrl(), middleware.JWT([]byte("RAHASIA")))
+	e.PUT("/projects/:id", pctrl.PutProjectsCtrl(), middleware.JWT([]byte("RAHASIA")))
 	e.DELETE("/projects/:id", pctrl.DeleteProjectsCtrl(), middleware.JWT([]byte("RAHASIA")))
 
 }
